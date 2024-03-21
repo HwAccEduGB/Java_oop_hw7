08b55aa1dc78d060d7560ff35faa30697e4e5892
benbatton83@gmail.com <benbatton83@gmail.com>
2024-03-21 19:24:52 +0300

    delete warnings

--- src/main/java/impl/Calculator.java
+++
@@ -1,9 +0,0 @@
-package impl;
-
-import model.ComplexNum;
-
-public interface Calculator {
-    ComplexNum mult(ComplexNum num1, ComplexNum num2);
-    ComplexNum add(ComplexNum num1, ComplexNum num2);
-    ComplexNum div(ComplexNum num1, ComplexNum num2);
-}
